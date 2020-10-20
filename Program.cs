23aa77926537b73962ffdacbdb9418ca9ec15005
Flávio José Formis <flavio.formis@pca.com.br>
2020-10-20 15:56:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NullableReferenceTypes
+{
+    #nullable enable
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person p = new Person(null!);
+        }
+
+        class Person { 
+            public Person(string firstName) => FirstName = firstName??throw new ArgumentNullException(nameof(firstName));
+            public string FirstName { get; } 
+        }
+    }
+}
